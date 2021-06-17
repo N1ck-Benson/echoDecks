54aa52f73abd829bad3d83bafa0b1064b5333071
Nick Benson <nk.benson@hotmail.com>
2021-06-17 15:04:34 +0100

    build Home component basics

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,36 @@
+import { Typography, Divider, Button } from "@material-ui/core";
+import { Link } from "@reach/router";
+import "../styles/Home.css";
+
 function Home() {
-  return <div>I'm the Home component</div>;
+  return (
+    <main>
+      <section>
+        <Typography variant="h6" className="welcome-heading">
+          Welcome to echoDecks!
+          <br />
+        </Typography>
+        <Typography variant="body1" className="welcome-text">
+          Hit start to begin generating flashcards and learning vocab more
+          effectively.
+        </Typography>
+        <div className="Button">
+          <Link to="add-deck" className="Link">
+            <Button variant="contained" color="primary">
+              Start
+            </Button>
+          </Link>
+        </div>
+      </section>
+      <Divider className="Divider" />
+      <Typography variant="h6" className="explainer-heading">
+        How does it work?
+      </Typography>
+      <Typography variant="body2" className="explainer-heading">
+        Explainer...
+      </Typography>
+    </main>
+  );
 }
 
 export default Home;
