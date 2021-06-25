893f077f03eceb030cd6d9c24dd2e439fed53696
Nick Benson <nk.benson@hotmail.com>
2021-06-25 12:34:27 +0100

    update httpLink to database credentials

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import {
 // with the echoDecks API; server uri should be
 // that of the API
 const httpLink = createHttpLink({
-  uri: env("DATABASE_URL"),
+  uri: "postgres://zpyujmlaimtwmm:af9552ddcf0ad6d2dc085af3dbac9d82cd3acc7355f5a73f208cb1d193bf98f1@ec2-3-218-71-191.compute-1.amazonaws.com:5432/d33001hvqck46n",
 });
 
 const client = new ApolloClient({
