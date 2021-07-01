61e86d87c2c95523c81b6a778d159ab6dd89277a
Nick Benson <nk.benson@hotmail.com>
2021-07-01 14:32:14 +0100

    get delete button working in view-decks

--- src/components/DeckListing.jsx
+++ src/components/DeckListing.jsx
@@ -1,7 +1,9 @@
 import { Paper, MenuItem, Menu } from "@material-ui/core";
 import { CheckCircleOutline, MoreHoriz } from "@material-ui/icons";
+import CancelOutlinedIcon from "@material-ui/icons/CancelOutlined";
 import { useState } from "react";
 import { Link } from "@reach/router";
+import { gql, useMutation } from "@apollo/client";
 import "../styles/ViewDecks.css";
 
 // Note: src and dst are included on props for future inclusion,
@@ -9,6 +11,7 @@ import "../styles/ViewDecks.css";
 const DeckListing = (props) => {
   const { id, title, createdAt, src, dst, isLearned } = props;
   const date = new Date(parseInt(createdAt)).toLocaleDateString();
+  const [isDeleted, setIsDeleted] = useState(false);
   const [anchorEl, setAnchorEl] = useState(null);
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -17,47 +20,70 @@ const DeckListing = (props) => {
     setAnchorEl(null);
   };
 
+  const DELETE_MUTATION = gql`
+    mutation deleteById {
+      delete (id: ${id}) {
+        title
+      }
+    }
+  `;
+
+  const [deleteById, { loading }] = useMutation(DELETE_MUTATION);
+
   // Note: schema is written for this, just need to write a useMutation() here.
-  const deleteDeck = () => {
-    console.log("User clicked 'delete deck'");
+  const handleDelete = () => {
+    deleteById(id).then(() => {
+      setIsDeleted(true);
+    });
   };
 
   return (
     <div className="view-card-stack">
-      <Paper className="view-top-card" elevation={3}>
-        <p className="deck-title">
-          {title}&nbsp;
-          <CheckCircleOutline
-            color={isLearned ? "primary" : "disabled"}
-            fontSize="small"
-          />
-        </p>
-        <button type="button" onClick={handleClick} className="button-wrapper">
-          <MoreHoriz />
-        </button>
-        <Menu
-          anchorEl={anchorEl}
-          keepMounted
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-        >
-          <MenuItem onClick={handleClose}>
-            <Link className="Link" to={`/learn-deck/${id}`}>
-              Learn Deck
-            </Link>
-          </MenuItem>
-          <MenuItem onClick={handleClose}>
-            <button
-              type="button"
-              className="button-wrapper"
-              onClick={deleteDeck}
-            >
-              Delete Deck
-            </button>
-          </MenuItem>
-        </Menu>
-        <p className="deck-date">{date}</p>
-      </Paper>
+      {!isDeleted ? (
+        <Paper className="view-top-card" elevation={3}>
+          <p className="deck-title">
+            {title}&nbsp;
+            <CheckCircleOutline
+              color={isLearned ? "primary" : "disabled"}
+              fontSize="small"
+            />
+          </p>
+          <button
+            type="button"
+            onClick={handleClick}
+            className="button-wrapper"
+          >
+            <MoreHoriz />
+          </button>
+          <Menu
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>
+              <Link className="Link" to={`/learn-deck/${id}`}>
+                Learn Deck
+              </Link>
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <button
+                type="button"
+                className="button-wrapper"
+                onClick={handleDelete}
+              >
+                Delete Deck
+              </button>
+            </MenuItem>
+          </Menu>
+          <p className="deck-date">{date}</p>
+        </Paper>
+      ) : (
+        <Paper className="view-top-card" elevation={3}>
+          <p className="deck-title">{title} was deleted</p>
+          <CancelOutlinedIcon />
+        </Paper>
+      )}
       <Paper className="view-middle-card" elevation={5}></Paper>
       <Paper className="view-bottom-card" elevation={3}></Paper>
       <span className="view-card-pocket"></span>
